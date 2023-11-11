c6ec092c78e0dd0c087d6557e08f9f770db2a04c
Kamal <kamalmiftah01@gmail.com>
2023-11-11 21:02:44 +0100

    Project: 0x11. C - printf, solution for task 1

--- _printf.c
+++ _printf.c
@@ -1,13 +1,21 @@
 #include "main.h"
 
+/**
+ *  _printf - a function that produces output according to a format.
+ *
+ * @format: a character string.
+ *
+ * Return: returns number of characters printed.
+*/
+
 int _printf(const char *format, ...)
 {
-	int i = 0, length;
+	int i = 0, length, value;
 	char *ptr;
 	char c;
 	va_list args;
-	va_start(args, format);
 
+	va_start(args, format);
 	if (format[0] == '%' && format[1] == '\0')
 		return (-1);
 	while (*format != '\0')
@@ -38,12 +46,28 @@ int _printf(const char *format, ...)
 				write(1, ptr, length);
 				i += length;
 			}
+			if (*format == 'd' || *format == 'i')
+			{
+				value = va_arg(args, int);
+				if (value < 0)
+					i++;
+				print_number(value);
+				i += _countDigits(value);
+			}
 		}
 		format++;
 	}
 	va_end(args);
 	return (i);
 }
+
+/**
+ * _strlen - a function that calculate the length of a string.
+ *
+ * @p: the string.
+ *
+ * Return: returns lenght;
+*/
 
 int _strlen(char *p)
 {
@@ -55,4 +79,52 @@ int _strlen(char *p)
 		p++;
 	}
 	return (i);
+}
+
+/**
+ * print_number - a function that converts an integer to a string
+ *			then it print it to standard output.
+ *
+ * @n: The number to print
+*/
+
+void print_number(int n)
+{
+	int number;
+	int num = n;
+	char signe;
+
+	if (n < 0)
+	{
+		signe = '-';
+		write(1, &signe, 1);
+
+		num = -num;
+	}
+	if (num > 9)
+	{
+		print_number(num / 10);
+	}
+	number = (num % 10 + '0');
+	write(1, &number, 1);
+}
+
+/**
+ *  _countDigits - a function that counts number of chars in an integer.
+ *
+ * @value: the integer.
+ *
+ * Return: return the count.
+*/
+
+int _countDigits(int value)
+{
+	int count = 0;
+
+	while (value != 0)
+	{
+		value /= 10;
+		count++;
+	}
+	return ((count == 0) ? 1 : count);
 }

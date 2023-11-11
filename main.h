584c68272a9ef408cbbe1d9333dd7a8c41c18e86
Kamal <kamalmiftah01@gmail.com>
2023-11-11 21:04:54 +0100

    adding file main.h

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef PRINT
+#define PRINT
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void print_number(int n);
+int _printf(const char *format, ...);
+int _strlen(char *p);
+int _countDigits(int value);
+
+#endif

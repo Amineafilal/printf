c8eb108539d75d99c2cfb7fdb5da827715d47232
Kamal <kamalmiftah01@gmail.com>
2023-11-13 22:22:57 +0100

    Project: 0x11. C - printf, fix for task 0

--- _print_string.c
+++ _print_string.c
@@ -6,10 +6,15 @@
  */
 int print_string(char *ptr)
 {
-	int count = 0, i = 0;
+	int count = 0;
 
 	if (ptr == NULL)
 		ptr = "(null)";
+		for (i = 0; i < _strlen(ptr); i++)
+		{
+			write(1, &ptr[0], 1);
+			return (ptr);
+		}
 	for (i = 0; ptr[i] != '\0'; i++)
 	{
 		write(1, &ptr[i], 1);

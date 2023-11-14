99ae3eba315b4f67d322e6015f21fafc16133ae3
Amineafilal <amineafilalelalami@gmail.com>
2023-11-14 15:50:26 -0800

    fixed main

--- main.h
+++ main.h
@@ -17,7 +17,7 @@ int print_string(char *ptr);
 int print_b(unsigned int n);
 int conv_oct(unsigned int n);
 int conv_hexa(unsigned int n);
-
+int print_u(unsigned int numb);
 int _countDigitsunsignedint(unsigned int value);
 void print_unsignedint(char *ptrunint);
 #endif
